8922f41e660e41c01e18b2467b89fc5007377afe
salmaaldakkak <115711172+salmaaldakkak@users.noreply.github.com>
2023-02-27 05:44:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+    'use strict';
+    $('.language span').on('click', function () {
+        $(':root').css('--mainColor', $(this).data('color'));
+    });
+});
+
+
+
+document.querySelectorAll('.gallery img').forEach(imge => {
+    imge.onclick = () => {
+        document.querySelector('.popup-photo').style.display = 'block';
+        document.querySelector('.popup-photo img').src = imge.getAttribute('src');
+
+    }
+});
+document.querySelector('.popup-photo span').onclick = () => {
+    document.querySelector('.popup-photo').style.display = 'none';
+}
+
+
+
+document.querySelectorAll('.pic img').forEach(imge => {
+    imge.onclick = () => {
+        document.querySelector('.popup-photo').style.display = 'block';
+        document.querySelector('.popup-photo img').src = imge.getAttribute('src');
+
+    }
+});
+document.querySelector('.popup-photo span').onclick = () => {
+    document.querySelector('.popup-photo').style.display = 'none';
+}
+
+
+
